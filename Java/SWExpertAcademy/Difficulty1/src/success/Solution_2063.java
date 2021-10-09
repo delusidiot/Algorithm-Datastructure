7b2993f4600daeeda7578fa9fd20ad8f7babd7ef
delusidiot <delusidiot@gmail.com>
2021-10-10 03:14:58 +0900

    [SWAcademy] 2063 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2063.java
@@ -0,0 +1,17 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_2063 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(line);
+		System.out.println(line[T / 2]);
+	}
+}

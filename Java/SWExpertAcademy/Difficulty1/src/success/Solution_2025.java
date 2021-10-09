9021564410c4e12b32ccb2393ecd81cddabe7377
delusidiot <delusidiot@gmail.com>
2021-10-10 04:48:50 +0900

    [SWAcademy] 2025 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2025.java
@@ -0,0 +1,17 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_2025 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Integer T = Integer.parseInt(br.readLine());
+		Integer total = 0;
+		for (int i = 1; i <= T; i++) {
+			total += i;
+		}
+		System.out.println(total);
+	}
+}

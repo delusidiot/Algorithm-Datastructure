82a2a465baba440959fe9559f653ce2f03568a12
delusidiot <delusidiot@gmail.com>
2021-10-10 03:41:16 +0900

    [SWAcademy] 2058 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2058.java
@@ -0,0 +1,14 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_2058 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().chars().map(c -> c - '0').sum());
+	}
+
+}

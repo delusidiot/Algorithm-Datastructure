7da299bdd47df293d7e4f0d9c424f96c5b5fe35d
delusidiot <delusidiot@gmail.com>
2021-10-10 04:13:06 +0900

    [SWAcademy] 2046 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2046.java
@@ -0,0 +1,14 @@
+package success;
+
+import java.util.Scanner;
+
+public class Solution_2046 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Integer T = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < T; i++) {
+			System.out.print("#");
+		}
+	}
+}

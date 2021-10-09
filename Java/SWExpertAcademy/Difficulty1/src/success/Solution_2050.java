c4db9d2edeefd8c43bd00702a21019b002b60b88
delusidiot <delusidiot@gmail.com>
2021-10-10 04:07:32 +0900

    [SWAcademy] 2050 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2050.java
@@ -0,0 +1,10 @@
+package success;
+
+import java.util.Scanner;
+
+public class Solution_2050 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine().chars().forEach(c -> System.out.print(c - 'A' + 1 + " "));
+	}
+}

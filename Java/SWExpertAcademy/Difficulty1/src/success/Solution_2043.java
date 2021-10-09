d2f230fd56ef2351710b7d61c148542c2de9484c
delusidiot <delusidiot@gmail.com>
2021-10-10 04:22:28 +0900

    [SWAcademy] 2043 Success (java)

---
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_2043.java
@@ -0,0 +1,11 @@
+package success;
+
+import java.util.Scanner;
+
+public class Solution_2043 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] num = sc.nextLine().split(" ");
+		System.out.println(Integer.parseInt(num[0]) - Integer.parseInt(num[1]) + 1);
+	}
+}

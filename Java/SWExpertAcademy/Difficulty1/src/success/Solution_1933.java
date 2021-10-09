1aafb42d1c7d3383e507d798cea8f6a0e07e495a
delusidiot <delusidiot@gmail.com>
2021-10-10 05:01:40 +0900

    [SWAcademy] 1933 Success (java)

--- Java/SWExpertAcademy/Difficulty1/src/success/Solution_1933.java
+++ Java/SWExpertAcademy/Difficulty1/src/success/Solution_1933.java
@@ -1,9 +1,6 @@
 package success;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class Solution_1933 {
 	public static void main(String[] args) throws IOException {

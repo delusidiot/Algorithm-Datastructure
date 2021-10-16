327cbb85c01f5332b52fb789c0546bf2293769a3
delusidiot <delusidiot@gmail.com>
2021-10-17 05:13:01 +0900

    [Programmers] Solution 42578 위장 succsess (java)

---
+++ Java/programmers/Difficulty2/src/success/Solution_42578.java
@@ -0,0 +1,20 @@
+package success;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution_42578 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int solution(String[][] clothes) {
+        return Arrays.stream(clothes)
+            .collect(Collectors.groupingBy(c -> c[1], Collectors.counting()))
+            .values().stream()
+            .collect(Collectors.reducing(1L, (answer, counter) -> answer * (counter + 1)))
+            .intValue() - 1;
+    }
+}

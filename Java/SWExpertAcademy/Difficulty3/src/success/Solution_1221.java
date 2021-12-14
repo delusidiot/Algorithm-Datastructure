cecc9e937b7c26a322a7c458968fa3cf7a81fe46
delusidiot <delusidiot@gmail.com>
2021-12-15 07:57:48 +0900

    [SWAcademy] Solution 1289 Meomory recovery(java)

---
+++ Java/SWExpertAcademy/Difficulty3/src/success/Solution_1221.java
@@ -0,0 +1,10 @@
+package success;
+
+public class Solution_1221 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java/SWExpertAcademy/Difficulty3/src/success/Solution_1289.java
+++ Java/SWExpertAcademy/Difficulty3/src/success/Solution_1289.java
@@ -3,7 +3,6 @@ package success;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution_1289 {
 

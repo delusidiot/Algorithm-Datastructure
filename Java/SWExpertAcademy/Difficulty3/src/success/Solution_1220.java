767245a595ff460a0af7a8ff5b5ad3ed15a2734b
delusidiot <delusidiot@gmail.com>
2021-12-14 22:43:53 +0900

    [SWAcademy] Solution 1220 Magnetic(java)

--- Java/SWExpertAcademy/Difficulty3/src/success/Solution_1220.java
+++ Java/SWExpertAcademy/Difficulty3/src/success/Solution_1220.java
@@ -1,10 +1,33 @@
 package success;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Solution_1220 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			String N = br.readLine();
+			int[][] table = new int[100][100];
+			for (int i = 0; i < 100; i++)
+				table[i] = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+			int count = 0;
+			int[] status = new int[100];
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < 100; j++) {
+					if(status[j] == 0 && table[i][j] == 1)
+						status[j] = table[i][j];
+					else if(status[j] == 1 && table[i][j] == 2) {
+						count++;
+						status[j] = 0;
+					}
+				}
+			}
+			System.out.printf("#%d %d\n", test_case, count);
+		}
 	}
 
 }

588adacb1599d9b4a8cb23db88a24da2c71664b8
delusidiot <delusidiot@gmail.com>
2021-12-28 04:01:49 +0900

    [Baekjoon] move(java)

---
+++ Java/Baekjoon/1000_1999/src/success/Main_1002.java
@@ -0,0 +1,43 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main_1002 {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine()); //x,y = -10000~10000 r<=10000
+		for (int testCase = 1; testCase <= T; testCase++) {
+			String[] line = br.readLine().split(" ");
+			int x1 = Integer.parseInt(line[0]);
+			int y1 = Integer.parseInt(line[1]);
+			int x2 = Integer.parseInt(line[2]);
+			int y2 = Integer.parseInt(line[3]);
+			int r1 = Integer.parseInt(line[4]);
+			int r2 = Integer.parseInt(line[5]);
+			int position = 0;
+			
+			long d = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);//�몢�젏 �궗�씠�쓽 嫄곕━ �젣怨�
+			long rsub = (r1-r2)*(r1-r2);
+			long radd = (r1+r2)*(r1+r2);
+
+			if(x1==x2&&y1==y2&&r1==r2) {
+				position = -1;
+			}else if(d == radd || d == rsub) {
+				position = 1;
+			}else if(rsub < d && d < radd) {
+				position = 2;
+			}else position = 0;
+			
+			bw.write(String.valueOf(position)+"\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
---
+++ Java/Baekjoon/1000_1999/src/success/Main_1003.java
@@ -0,0 +1,42 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main_1003 {
+	static int[] p0;
+	static int[] p1;
+	static final int max =41;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		p0=new int[max];
+		p1=new int[max];
+		for (int i = 0; i < max; i++) {
+			fibo2(i);
+		}
+		int T = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = Integer.parseInt(br.readLine());
+			bw.write(String.valueOf(p0[N])+" "+String.valueOf(p1[N])+"\n");
+			bw.flush();
+		}
+		br.close();
+		bw.close();
+	}
+	public static void fibo2(int n){
+		if(n==0) {
+			p0[0]=1;
+			p1[0]=0;
+		}else if(n ==1) {
+			p0[1]=0;
+			p1[1]=1;
+		}else {
+			p0[n]=p0[n-2]+p0[n-1];
+			p1[n]=p1[n-2]+p1[n-1];
+		}
+	}
+}

4ee353c87f97b1306835108728ab67147819f686
delusidiot <delusidiot@gmail.com>
2022-01-01 19:20:31 +0900

    [Baekjoon] 1463 1로 만들기(java)

---
+++ Java/Baekjoon/1000_1999/src/success/Main_1463.java
@@ -0,0 +1,27 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_1463 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Integer X = Integer.parseInt(br.readLine());
+		int arr[] = new int[1000001];
+		
+		for (int i = 2; i <= X; i++) {
+			arr[i] = arr[i - 1] + 1;
+			if (i%2 == 0)
+				arr[i] = Math.min(arr[i],  arr[i / 2] + 1);
+			if (i%3 == 0)
+				arr[i] = Math.min(arr[i],  arr[i / 3] + 1);
+			
+		}
+		for (int i = 1; i <= X; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println(arr[X]);
+	}
+}

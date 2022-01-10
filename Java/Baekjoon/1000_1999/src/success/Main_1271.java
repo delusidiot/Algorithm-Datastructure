58e4e26b218f28559dbdd41def75004b1a96f8c1
delusidiot <delusidiot@gmail.com>
2022-01-10 23:12:06 +0900

    [Baekjoon] 1271 엄청난부자2 (java)

---
+++ Java/Baekjoon/1000_1999/src/success/Main_1271.java
@@ -0,0 +1,15 @@
+package success;
+
+import java.util.Scanner;
+
+public class Main_1271 {
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(N / M);
+		System.out.println(N % M);
+	}
+
+}

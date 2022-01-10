b845b1e3457430510594909b20e8ba4a6860c1f4
delusidiot <delusidiot@gmail.com>
2022-01-11 00:01:16 +0900

    [Baekjoon] 1271 엄청난부자2 (java)

--- Java/Baekjoon/1000_1999/src/success/Main_1271.java
+++ Java/Baekjoon/1000_1999/src/success/Main_1271.java
@@ -1,15 +1,18 @@
 package success;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main_1271 {
-	static Scanner sc;
-	public static void main(String[] args) {
-		sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		System.out.println(N / M);
-		System.out.println(N % M);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] line = br.readLine().trim().split(" ");
+		BigInteger N = new BigInteger(line[0]);
+		BigInteger M = new BigInteger(line[1]);
+		System.out.println(N.divide(M));
+		System.out.println(N.remainder(M));
 	}
-
 }
+

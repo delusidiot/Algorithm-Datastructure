8af0d7a9f53d6779118eb2b3c58f555a0885caa7
delusidiot <delusidiot@gmail.com>
2022-01-12 01:42:56 +0900

    [Baekjoon] 1592 영식이와 친구들(java)

---
+++ Java/Baekjoon/1000_1999/src/success/Main_1592.java
@@ -0,0 +1,26 @@
+package success;
+
+import java.util.Scanner;
+
+public class Main_1592 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int L = sc.nextInt();
+		int[] Ncatch = new int[N];
+		int i = 0;
+		int count = 0;
+		while(true) {
+			Ncatch[i]++;
+			if(Ncatch[i]==M)break;
+			count++;
+			if(Ncatch[i]%2==1) {
+				i=(i+N-L)%N;
+			}else {
+				i=(i+L)%N;
+			}
+		}
+		System.out.println(count);
+	}
+}

98ba2a112cde2dd24cc0f8caac538663701d938b
delusidiot <delusidiot@gmail.com>
2022-04-18 13:00:20 +0900

    [Programmers] 숫자 문자열과 영단어 success

--- Java/programmers/Level1/src/success/Solution_81301.java
+++ Java/programmers/Level1/src/success/Solution_81301.java
@@ -1,5 +1,20 @@
 package success;
 
-public class Solution_81301 {
+/**
+ * 숫자 문자열과 영단어
+ * 
+ * @author delusidiot
+ *
+ */
 
+public class Solution_81301 {
+	private final String[] numbersAlpha =
+		{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+	public int solution(String s) {
+		for (int i = 0; i < numbersAlpha.length; i++) {
+			s = s.replaceAll(numbersAlpha[i], String.valueOf(i));
+		}
+		return Integer.parseInt(s);
+	}
 }

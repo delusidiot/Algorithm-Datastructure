4f74234612db6785f0b70d5812c10c0b29e6d90d
delusidiot <delusidiot@gmail.com>
2022-04-18 14:48:44 +0900

    [Programmers] 약수의 개수와 덧셈 success

--- Java/programmers/Level1/src/success/Solution_77884.java
+++ Java/programmers/Level1/src/success/Solution_77884.java
@@ -5,6 +5,12 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.IntStream;
 
+/**
+ * ¾à¼öÀÇ °³¼ö¿Í µ¡¼À
+ * @author delusidiot
+ *
+ */
+
 public class Solution_77884 {
 	
 	private Set<Integer> set = new HashSet<>(Arrays.asList(1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900, 961));

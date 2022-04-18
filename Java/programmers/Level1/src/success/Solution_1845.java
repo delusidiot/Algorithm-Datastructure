9421e6181c6223935412cd4cf4f7ddb0cfece00d
delusidiot <delusidiot@gmail.com>
2022-04-18 14:21:43 +0900

    [Programmers] 폰켓몬 success

---
+++ Java/programmers/Level1/src/success/Solution_1845.java
@@ -0,0 +1,23 @@
+package success;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+/**
+ * ÆùÄÏ¸ó
+ * @author delusidiot
+ *
+ */
+
+public class Solution_1845 {
+
+	public static void main(String[] args) {
+		Solution_1845 solution = new Solution_1845();
+		int[] nums = {3,1,2,3};
+		System.out.println(solution.solution(nums));
+	}
+	public int solution(int[] nums) {
+		Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return (nums.length / 2 <= set.size())? nums.length / 2: set.size();
+    }
+}

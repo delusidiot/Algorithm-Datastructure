4922b389e1e440c87d4c88fdab404aee65757953
delusidiot <delusidiot@gmail.com>
2022-04-19 20:56:37 +0900

    [Programmers] 직사각형 별찍기

---
+++ Java/programmers/Level1/src/success/Solution_12969.java
@@ -0,0 +1,24 @@
+package success;
+
+import java.util.Scanner;
+
+/**
+ * 직사각형 별찍기
+ * @author delusidiot
+ *
+ */
+public class Solution_12969 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        for (int i = 0; i < b; i++) {
+        	for (int j = 0; j < a; j++) {
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+    }
+}

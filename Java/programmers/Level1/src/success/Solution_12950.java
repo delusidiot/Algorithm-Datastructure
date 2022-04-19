f308e2b2d4be290e59b45a4e598a302815323d0a
delusidiot <delusidiot@gmail.com>
2022-04-19 20:48:45 +0900

    [Programmers] 행렬의 덧셈

---
+++ Java/programmers/Level1/src/success/Solution_12950.java
@@ -0,0 +1,26 @@
+package success;
+
+/**
+ * Çà·ÄÀÇ µ¡¼À
+ * @author delusidiot
+ *
+ */
+public class Solution_12950 {
+
+	public static void main(String[] args) {
+		Solution_12950 solution = new Solution_12950();
+		int[][] arr1 = {{1, 2},{2, 3}};
+		int[][] arr2 = {{3,4},{5,6}};
+		System.out.println(solution.solution(arr1, arr2));
+
+	}
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+        	for (int j = 0; j < arr1[i].length; j++) {
+        		answer[i][j] = arr1[i][j] + arr2[i][j];
+        	}
+        }
+        return answer;
+    }
+}

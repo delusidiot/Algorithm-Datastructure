79a175ea1ef87d57fc069e1b535f55d15c17a80d
delusidiot <delusidiot@gmail.com>
2022-04-19 13:46:52 +0900

    [Programmers] 두 개 뽑아서 더하기

---
+++ Java/programmers/Level1/src/success/Solution_68644.java
@@ -0,0 +1,31 @@
+package success;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 두 개 뽑아서 더하기
+ * @author delusidiot
+ *
+ */
+
+public class Solution_68644 {
+
+	public static void main(String[] args) {
+		Solution_68644 soution = new Solution_68644();
+		int[] numbers = {2,1,3,4,1};
+		System.out.println(Arrays.toString(soution.solution(numbers)));
+
+	}
+
+	public int[] solution(int[] numbers) {
+        Set<Integer> set = new TreeSet<Integer>();
+        for (int i = 0; i < numbers.length -1; i++) {
+        	for (int j = i + 1; j < numbers.length; j++) {
+        		set.add(numbers[i] + numbers[j]);
+        	}
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

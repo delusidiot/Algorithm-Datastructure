6322b899919c673b4d4748637250e7316afb7321
delusidiot <delusidiot@gmail.com>
2022-04-19 15:13:13 +0900

    [Programmers] 두 정수 사이의 합

---
+++ Java/programmers/Level1/src/success/Solution_12912.java
@@ -0,0 +1,17 @@
+package success;
+/**
+ * 두 정수 사이의 합
+ * @author delusidiot
+ *
+ */
+public class Solution_12912 {
+
+	public static void main(String[] args) {
+		Solution_12912 solution = new Solution_12912();
+		System.out.println(solution.solution(3, 5));
+
+	}
+	public long solution(int a, int b) {
+        return (long)(Math.abs(a - b) + 1) * (a + b) / 2;
+    }
+}

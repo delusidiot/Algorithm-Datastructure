e85c8c207d0583497562a8ac254765944fc6e68b
delusidiot <delusidiot@gmail.com>
2022-04-19 14:18:35 +0900

    [Programmers] 최소 직사각형

---
+++ Java/programmers/Level1/src/success/Solution_86491.java
@@ -0,0 +1,25 @@
+package success;
+
+/**
+ * 최소 직사각형
+ * @author delusidiot
+ *
+ */
+
+public class Solution_86491 {
+	public static void main(String[] args) {
+		Solution_86491 solution = new Solution_86491();
+		int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+		System.out.println(solution.solution(sizes));
+	}
+	public int solution(int[][] sizes) {
+		int widthMax = 0;
+        int heightMax = 0;
+        for (int i = 0; i < sizes.length; i++) {
+        	widthMax = Math.max(widthMax, Math.max(sizes[i][0], sizes[i][1]));
+        	heightMax = Math.max(heightMax, Math.min(sizes[i][0], sizes[i][1]));
+        }
+        
+        return heightMax * widthMax;
+    }
+}

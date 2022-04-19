5708861acfeb3dabb2e5bacdd114500180df3b21
delusidiot <delusidiot@gmail.com>
2022-04-19 20:21:37 +0900

    [Programmers] 짝수와 홀수

---
+++ Java/programmers/Level1/src/success/Solution_12937.java
@@ -0,0 +1,18 @@
+package success;
+
+/**
+ * Â¦¼ö¿Í È¦¼ö
+ * @author delusidiot
+ *
+ */
+public class Solution_12937 {
+
+	public static void main(String[] args) {
+		Solution_12937 solution = new Solution_12937();
+		System.out.println(solution.solution(3));
+	}
+	public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+}

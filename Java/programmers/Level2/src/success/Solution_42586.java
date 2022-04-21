16460cb1511f1e855f57f0c2abf3b2244f06699a
delusidiot <delusidiot@gmail.com>
2022-04-21 18:50:32 +0900

    [Programmers] 기능 개발

--- Java/programmers/Level2/src/success/Solution_42586.java
+++ Java/programmers/Level2/src/success/Solution_42586.java
@@ -1,12 +1,18 @@
 package success;
 
 import java.util.*;
-
+/**
+ * 기능 개발
+ * @author delusidiot
+ *
+ */
 public class Solution_42586 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution_42586 solution = new Solution_42586();
+		int[] progresses = {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		System.out.println(Arrays.toString(solution.solution2(progresses, speeds)));
 	}
 	public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> list = new ArrayList<Integer>();
@@ -22,4 +28,25 @@ public class Solution_42586 {
         }
         return list.stream().mapToInt(i -> i).toArray();
     }
+	
+	public int[] solution2 (int[] prograss, int[] speeds) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < prograss.length; i++) {
+			list.add((int)Math.ceil((double)(100 - prograss[i]) / speeds[i]));
+		}
+		List<Integer> result = new ArrayList<Integer>();
+		int day = list.remove(0);
+		int counter = 1;
+		for (Integer num: list) {
+			if (day < num) {
+				result.add(counter);
+				day = num;
+				counter = 1;
+			}
+			else
+				counter++;
+		}
+		result.add(counter);
+		return result.stream().mapToInt(Integer::intValue).toArray();
+	}
 }

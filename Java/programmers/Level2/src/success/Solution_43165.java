270ddf6f3732f665cc1c32cba918bb584d468ceb
delusidiot <delusidiot@gmail.com>
2022-04-22 16:33:26 +0900

    [programmers] 타겟 넘버

---
+++ Java/programmers/Level2/src/success/Solution_43165.java
@@ -0,0 +1,27 @@
+package success;
+/**
+ * Å¸°Ù ³Ñ¹ö
+ * @author delusidiot
+ *
+ */
+public class Solution_43165 {
+	public static void main(String[] args) {
+		
+	}
+	public int solution(int[] numbers, int target) {
+        int answer = dfs(numbers, target, 0, 0);
+        return answer;
+    }
+	
+	public int dfs(int[] numbers, int target, int start, int sum) {
+		if (start == numbers.length)
+		{
+			if (target == sum)
+				return 1;
+			return 0;
+		}
+		int p = dfs(numbers, target, start + 1, sum + numbers[start]);
+		int m = dfs(numbers, target, start + 1, sum - numbers[start]);
+		return p + m;
+	}
+}

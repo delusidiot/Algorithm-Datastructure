a150a7351787735fe953a5c0bbdcc3c2a521aca3
delusidiot <delusidiot@gmail.com>
2022-04-27 17:04:41 +0900

    [Programmers] 영어 끝말잇기

---
+++ Java/programmers/Level2/src/success/Solution_12981.java
@@ -0,0 +1,47 @@
+package success;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 영어 끝말잇기
+ * @author delusidiot
+ *
+ */
+public class Solution_12981 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+		Solution_12981 solution = new Solution_12981();
+		System.out.println(Arrays.toString(solution.solution(n, words)));
+		n = 5;
+		String[] words2 = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+		System.out.println(Arrays.toString(solution.solution(n, words2)));
+		n = 2;
+		String[] words3 = {"hello", "one", "even", "never", "now", "world", "draw"};
+		System.out.println(Arrays.toString(solution.solution(n, words3)));
+		
+	}
+	
+	public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Set<String> set = new HashSet<String>();
+        int i = 1;
+        boolean check = true;
+        set.add(words[0]);
+        while (i < words.length && check) {
+        	if (words[i - 1].charAt(words[i-1].length() -1) != words[i].charAt(0) || !set.add(words[i])) {
+        		check = false;
+        		break;
+        	}
+        	i++;
+        }
+        if (i == words.length && check)
+        	return new int[] {0,0};
+        answer[0] = i % n + 1;
+        answer[1] = i / n + 1;
+        return answer;
+    }
+}

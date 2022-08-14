0c2c2c83eb38ec79fcf69862fbd33529a040915b
delusidiot <delusidiot@gmail.com>
2022-08-14 16:35:46 +0900

    [Baekjoon] 1998년생인 내가 태국에서는 2541년생?! success

---
+++ Java/Baekjoon/18000_18999/src/success/Main_18108.java
@@ -0,0 +1,12 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_18108 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) - 543);
+	}
+}
